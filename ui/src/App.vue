45412db9377ec39cbeb85ed129f5180ce185d3f5
Васильев Виталий <vasilev.v.vitalii@gmail.com>
2021-12-02 12:15:26 +0300

    ...

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,8 +1,7 @@
 <template>
     <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+        <router-link to="/">Home</router-link> | <router-link to="/dashboard">Dashboard</router-link> | <router-link to="/history">History</router-link> |
+        <router-link to="/edit">Edit</router-link>
     </div>
     <router-view />
 </template>
-

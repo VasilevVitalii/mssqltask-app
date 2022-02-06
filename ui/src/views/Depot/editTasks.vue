b7dd7902bf2da197c7dd48307270f1a2cf5ed63b
Vitalii Vasilev <vasilev.v.vitalii@gmail.com>
2022-02-06 18:16:15 +0300

    ...

--- ui/src/views/Depot/editTasks.vue
+++ ui/src/views/Depot/editTasks.vue
@@ -15,6 +15,7 @@
                     {name: 'key', label: 'uniq key', field: 'item.state.key', sortable: true, style: 'width: 200px', align: 'left'},
                     {name: 'allow', label: 'enabled, allow rows and messages', style: 'width: 200px', align: 'left'},
                     {name: 'schedule', label: 'schedule', field: 'item.state.metronom', style: 'width: 200px', align: 'left'},
+                    {name: 'queries', label: 'queries', field: 'item.state.queries', style: 'width: 100px', align: 'left'},
                 ]">
             <template v-slot:body="props">
                 <q-tr :props="props" :class="props.row.isDel ? 'bg-negative-light' : props.row.isNew || props.row.item.getUpdProps().length > 0 ? 'bg-positive-light' : undefined">
@@ -48,24 +49,49 @@
                             </q-btn-dropdown>
                             <q-input v-show="props.row.item.state.metronom.kind === 'cron'" borderless placeholder="text cron" v-model="props.row.item.state.metronom.cron"/>
                             <q-btn-dropdown dense color="accent" flat label="edit" v-show="props.row.item.state.metronom.kind === 'custom'">
-                                <div>
-                                    <q-checkbox v-model="props.row.item.state.metronom.weekdaySun" label="sunday" />
-                                    <q-checkbox v-model="props.row.item.state.metronom.weekdayMon" label="monday" />
-                                    <q-checkbox v-model="props.row.item.state.metronom.weekdayTue" label="tuesday" />
-                                    <q-checkbox v-model="props.row.item.state.metronom.weekdayWed" label="wednesday" />
-                                    <q-checkbox v-model="props.row.item.state.metronom.weekdayThu" label="thursday" />
-                                    <q-checkbox v-model="props.row.item.state.metronom.weekdayFri" label="friday" />
-                                    <q-checkbox v-model="props.row.item.state.metronom.weekdaySat" label="saturday" />
-                                    <q-separator style="margin: 5px 10px 5px 10px" color="primary" />
-                                    <q-btn-toggle
-                                        v-model="props.row.item.state.metronom.periodicity"
-                                        toggle-color="accent" flat dense
-                                        :options="[{label: 'every', value: 'every'},{label: 'once', value: 'once'}]"
-                                    />
-                                    <q-input borderless label="period in minutes" v-model="props.row.item.state.metronom.periodMinutes"/>
+                                <div style="margin: 10px; display: grid">
+                                    <q-checkbox :false-value="false || null" v-model="props.row.item.state.metronom.weekdaySun" label="sunday" style="margin: 0px 0px 0px 30px" />
+                                    <q-checkbox :false-value="false || null" v-model="props.row.item.state.metronom.weekdayMon" label="monday" style="margin: 0px 0px 0px 30px" />
+                                    <q-checkbox :false-value="false || null" v-model="props.row.item.state.metronom.weekdayTue" label="tuesday" style="margin: 0px 0px 0px 30px" />
+                                    <q-checkbox :false-value="false || null" v-model="props.row.item.state.metronom.weekdayWed" label="wednesday" style="margin: 0px 0px 0px 30px" />
+                                    <q-checkbox :false-value="false || null" v-model="props.row.item.state.metronom.weekdayThu" label="thursday" style="margin: 0px 0px 0px 30px" />
+                                    <q-checkbox :false-value="false || null" v-model="props.row.item.state.metronom.weekdayFri" label="friday" style="margin: 0px 0px 0px 30px" />
+                                    <q-checkbox :false-value="false || null" v-model="props.row.item.state.metronom.weekdaySat" label="saturday" style="margin: 0px 0px 0px 30px" />
+                                    <div style="display: flex; align-items: center">
+                                        <div style="width: 100px">
+                                            <q-btn-dropdown dense color="accent" flat :label="props.row.item.state.metronom.periodicity || '???'">
+                                                <q-list>
+                                                    <q-item clickable v-close-popup @click="props.row.item.state.metronom.periodicity = 'every'">
+                                                    <q-item-section>
+                                                        <q-item-label>EVERY</q-item-label>
+                                                    </q-item-section>
+                                                    </q-item>
+
+                                                    <q-item clickable v-close-popup @click="props.row.item.state.metronom.periodicity = 'once'">
+                                                    <q-item-section>
+                                                        <q-item-label>ONCE</q-item-label>
+                                                    </q-item-section>
+                                                    </q-item>
+                                                </q-list>
+                                            </q-btn-dropdown>
+                                        </div>
+                                        <q-input
+                                            borderless
+                                            style="width: 130px"
+                                            v-model="props.row.item.state.metronom.periodMinutes"
+                                            :label="'minutes' + (props.row.item.state.metronom.periodicity === 'every' ? ' every hour' : props.row.item.state.metronom.periodicity === 'once' ? ' after midnight' : '')"
+                                        />
+                                    </div>
                                 </div>
                             </q-btn-dropdown>
                         </div>
+                    </q-td>
+                    <q-td key="schedule" :props="props">
+                        <q-btn-dropdown dense color="accent" flat label="edit">
+                            <div>
+                                <q-btn dense flat color="accent" size="sm" style="margin: 0px 0px 0px 5px" label="add"/>
+                            </div>
+                        </q-btn-dropdown>
                     </q-td>
                 </q-tr>
             </template>
